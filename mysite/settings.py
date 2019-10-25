2e58849518ee90c7008899ac50f4b019d378d0c2
aki1502 <aki1502.adm@gmail.com>
2019-10-25 15:45:16 +0900

    Phone version site view

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=kvldc_)um)qnby^f@6y+2!^8y^m&vnxemj!atgxylfoz59^fm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", ".pythonanywhere.com", "www.aki1502.tk"]
 
